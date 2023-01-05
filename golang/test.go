f58358b20cb0b36234d9a2c4d01f1bc07b54e54f
rasel <rasel@appscode.com>
2023-01-05 15:08:24 +0600

    map pass by value
    Signed-off-by: rasel <rasel@appscode.com>

--- golang/test.go
+++ golang/test.go
@@ -1,7 +1,32 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
+
+func formating() {
 	s := "Rasel"
 	println(`
 hi! my name is `, s, ` - hello `, s)
+}
+
+func sudoFunc(m *map[int]int) {
+	*m = make(map[int]int)
+}
+
+func reference() {
+	var m map[int]int // m is nil
+	//m := make(map[int]int) // m is not nil
+
+	sudoFunc(&m)
+
+	if m == nil {
+		fmt.Println("nil")
+	} else {
+		fmt.Println("not nil")
+	}
+}
+
+func main() {
+	reference()
 }
