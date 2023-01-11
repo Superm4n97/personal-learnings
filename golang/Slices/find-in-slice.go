aba24989e785ce9bab255873beea039db6fbbf18
rasel <rasel@appscode.com>
2023-01-11 19:18:30 +0600

    search in slice added
    Signed-off-by: rasel <rasel@appscode.com>

---
+++ golang/Slices/find-in-slice.go
@@ -0,0 +1,11 @@
+package main
+
+func findInSlice() {
+	//sl := []string{"hello","rasel"}
+	//sort.SearchStrings(sl,"hi")
+	// you can search a element in a sorted slice otherwise you have to iterate the whole slice
+}
+
+func main() {
+	findInSlice()
+}
