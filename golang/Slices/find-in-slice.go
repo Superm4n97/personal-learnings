cc3c3970984be9735bb52579b2918669d775a849
rasel <rasel@appscode.com>
2023-01-12 10:14:29 +0600

    findByMap added
    Signed-off-by: rasel <rasel@appscode.com>

--- golang/Slices/find-in-slice.go
+++ golang/Slices/find-in-slice.go
@@ -1,11 +1,24 @@
 package main
 
+import "fmt"
+
 func findInSlice() {
 	//sl := []string{"hello","rasel"}
 	//sort.SearchStrings(sl,"hi")
 	// you can search a element in a sorted slice otherwise you have to iterate the whole slice
 }
 
+func findByMap() {
+	mp := make(map[string]int)
+	sl := []string{"foo", "bar"}
+	for _, st := range sl {
+		mp[st] = 1
+	}
+	fmt.Println(mp["foo"])
+	fmt.Println(mp["hi"])
+}
+
 func main() {
 	findInSlice()
+	findByMap()
 }
